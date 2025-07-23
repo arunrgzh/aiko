751200e88da115dafbeebfc7b00a161224145a54
Aruzhan Nurgozha <aruzhan.nurgozha@nu.edu.kz>
2025-07-23 11:10:26 +0500

    setup alembic

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,1 +1,11 @@
-# Database Models Package 
+# Database Models Package
+from app.database import Base
+
+# import all modules in app.models package so their models register
+import importlib, pkgutil, pathlib, sys
+
+package = pathlib.Path(__file__).parent
+for _, modname, _ in pkgutil.iter_modules([str(package)]):
+    if modname.startswith("_"):  # skip private modules
+        continue
+    importlib.import_module(f"app.models.{modname}")
