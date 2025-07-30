76ea75c4f2e87e34c77ff3eb3e92e46611368c71
Aruzhan Nurgozha <aruzhan.nurgozha@nu.edu.kz>
2025-07-30 22:28:04 +0500

    fix auth.error

--- front/src/auth.ts
+++ front/src/auth.ts
@@ -3,11 +3,6 @@ import appConfig from '@/configs/app.config'
 import authConfig from '@/configs/auth.config'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    pages: {
-        signIn: appConfig.signInEntryPath,
-        error: '/auth/error',
-    },
-
     session: {
         strategy: 'jwt',
         maxAge: 30 * 24 * 60 * 60, // 30 days
